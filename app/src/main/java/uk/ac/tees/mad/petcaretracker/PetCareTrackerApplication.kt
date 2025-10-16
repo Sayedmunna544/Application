204c70dc986118c5396785e1cd142b6f9cc181b2
Sayedmunna <sayedmunna39@gmail.com>
2025-10-16 12:00:00 +0530

    Implemented dependency injection with hilt and folder initialization

---
+++ app/src/main/java/uk/ac/tees/mad/petcaretracker/PetCareTrackerApplication.kt
@@ -0,0 +1,8 @@
+package uk.ac.tees.mad.petcaretracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PetCareTrackerApplication : Application() {
+}
