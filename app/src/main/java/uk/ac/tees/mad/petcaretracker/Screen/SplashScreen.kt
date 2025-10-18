b80a45c4946ce643d87c0f8cb2e2bf8177f051aa
Sayedmunna <sayedmunna39@gmail.com>
2025-10-18 12:00:00 +0530

    Created Splash Screen with app logo

--- app/src/main/java/uk/ac/tees/mad/petcaretracker/Screen/SplashScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/petcaretracker/Screen/SplashScreen.kt
@@ -1,7 +1,43 @@
 package uk.ac.tees.mad.petcaretracker.Screen
 
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import uk.ac.tees.mad.petcaretracker.R
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(innerPadding: PaddingValues) {
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(R.drawable.app_icon), contentDescription = null,
+            modifier = Modifier
+                .shadow(elevation = 24.dp, shape = RoundedCornerShape(42.dp))
+                .size(184.dp)
+                .clip(RoundedCornerShape(42.dp))
+                .border(2.dp, color = Color.Black, shape = RoundedCornerShape(42.dp))
+
+
+        )
+    }
 }
